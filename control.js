a36bad1d4542cca855ad5c63eddc58506b1834eb
Eric Hendrickson <hendrickson02@gmail.com>
2016-11-25 22:03:32 -0800

    Managed to wrap every <p> element's words into <span>(word)</span>, and each span disappears when clicked and reappears when clicked a second time.

--- control.js
+++ control.js
@@ -1,9 +1,38 @@
 $(document).ready(function() {
-  $("p").on("click", function() {
-    if ($(this).hasClass("blend")) {
-      alert("has class");
+  // Add spans to the <p> elements
+  makeSpans();
+
+  // The idea here is that if you click an element, its text opacity goes to
+  // 0.0, making it totally transparent. But if you click it again, it goes
+  // back to normal
+  $("span").on("click", function() {
+    if($(this).hasClass("hideWord")) {
+      $(this).removeClass("hideWord");
     } else {
-      alert("doesn't have class");
+      $(this).addClass("hideWord");
     }
+
   });
+
 });
+
+// Takes a paragraph and puts spans in each of them. Credit for this idea
+// goes to Daniel Tonon (http://stackoverflow.com/a/20850348)
+function makeSpans() {
+  // For each <p> element
+  $("p").each(function() {
+    // take all the words and put them in an array
+    var words = $(this).text().split(" ");
+
+    // empty the original <p> element
+    $(this).empty();
+
+    // then append every word in the array into the <p>, except this time
+    // wrap them with <span>...</span>
+    for (var i = 0; i < words.length; i++) {
+      $(this).append("<span>" + words[i] + " </span>");
+    }
+
+  });
+
+}
