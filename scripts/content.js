2f93e4b55eb5f0c95369b6e1a099ce84135b1942
Jeremy Scott <j5scott@ucsd.edu>
2016-11-29 17:19:07 -0800

    fixed a bug that occured when the page had no style section to begin with, by adding jquery to add <style> if it weren't detected

--- scripts/content.js
+++ scripts/content.js
@@ -1,37 +1,39 @@
-  $('style').append('.hideWord{color:blue;background-color:blue}.hideWord2{color:white;background-color:white}.hoverOver{color:red;opacity:1;background-color:white}.whiteBG{background-color:white!important}.imageHide{opacity:0.02!important}.imageReveal{opacity:1.0!important}.imageReveal:parent{background-color:white!important}.imageHide:parent{background-color:blue!important}');
-  
-  $('body').after('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script><script>function makeSpans(){$("h1, h2, h3, h4, h5, h6, p, li, td, img").each(function(){var s=splitText($(this).html());$(this).empty();for(var a="",h=0;h<s.length;h++)a+=s[h];$(this).html(a)})}function splitText(s){for(var a=[],h="",e=0;e<s.length;e++)if("<"===s.charAt(e)){a.push("<span>"+h+"</span>"),h="<";var i=1;do e++,"<"===s.charAt(e)&&i++,">"===s.charAt(e)&&i--,h+=s.charAt(e);while(i>0);console.log(h),a.push(h),h=""}else" "!=s.charAt(e)&&"."!=s.charAt(e)?h+=s.charAt(e):(console.log(h),a.push("<span>"+h+"</span>"),h="",a.push(""+s.charAt(e)));return a}$(document).ready(function(){makeSpans(),$("BODY").addClass("whiteBG"),$("span").on("click",function(){$(this).hasClass("hideWord")?($(this).removeClass("hideWord hoverOver"),$(this).addClass("hideWord2")):$(this).hasClass("hideWord2")?$(this).removeClass("hideWord2"):$(this).addClass("hideWord")}),$("span").hover(function(){$(this).hasClass("hideWord")&&$(this).addClass("hoverOver")},function(){$(this).removeClass("hoverOver")}),$("img").on("click",function(){$(this).hasClass("imageHide")?($(this).removeClass("imageHide"),$(this).addClass("imageReveal")):$(this).addClass("imageHide")}),$("img").hover(function(){$(this).hasClass("imageHide")&&$(this).addClass("imageReveal")},function(){$(this).removeClass("imageReveal")})});</script>');
 
 
 $(document).ready(function() {
   // Add spans to the <p> elements
   makeSpans();
-  
-  //make background white 
-  $('BODY').addClass("whiteBG");
-  
-  //add jQuery to top of html doc
 
-  
-  
+  //make background white
+  $('BODY').addClass("whiteBG");
+
+  if(!$('style').length){
+    $('head').append('<style></style>');
+  }
+  //add jQuery to top of html doc
+  $('style').append('.hideWord{color:blue;background-color:blue}.hideWord2{color:white;background-color:white}.hoverOver{color:red;opacity:1;background-color:white}.whiteBG{background-color:white!important}.imageHide{opacity:0.02!important}.imageReveal{opacity:1.0!important}.imageReveal:parent{background-color:white!important}.imageHide:parent{background-color:blue!important}');
+
+  $('body').after('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script><script>function makeSpans(){$("h1, h2, h3, h4, h5, h6, p, li, td, img").each(function(){var s=splitText($(this).html());$(this).empty();for(var a="",h=0;h<s.length;h++)a+=s[h];$(this).html(a)})}function splitText(s){for(var a=[],h="",e=0;e<s.length;e++)if("<"===s.charAt(e)){a.push("<span>"+h+"</span>"),h="<";var i=1;do e++,"<"===s.charAt(e)&&i++,">"===s.charAt(e)&&i--,h+=s.charAt(e);while(i>0);console.log(h),a.push(h),h=""}else" "!=s.charAt(e)&&"."!=s.charAt(e)?h+=s.charAt(e):(console.log(h),a.push("<span>"+h+"</span>"),h="",a.push(""+s.charAt(e)));return a}$(document).ready(function(){makeSpans(),$("BODY").addClass("whiteBG"),$("span").on("click",function(){$(this).hasClass("hideWord")?($(this).removeClass("hideWord hoverOver"),$(this).addClass("hideWord2")):$(this).hasClass("hideWord2")?$(this).removeClass("hideWord2"):$(this).addClass("hideWord")}),$("span").hover(function(){$(this).hasClass("hideWord")&&$(this).addClass("hoverOver")},function(){$(this).removeClass("hoverOver")}),$("img").on("click",function(){$(this).hasClass("imageHide")?($(this).removeClass("imageHide"),$(this).addClass("imageReveal")):$(this).addClass("imageHide")}),$("img").hover(function(){$(this).hasClass("imageHide")&&$(this).addClass("imageReveal")},function(){$(this).removeClass("imageReveal")})});</script>');
+
+
   // The idea here is that if you click an element, its text opacity goes to
   // 0.0, making it totally transparent. But if you click it again, it goes
   // back to normal
-  
-  
+
+
   $("span").on("click", function() {
     if ($(this).hasClass("hideWord")) {
       $(this).removeClass("hideWord hoverOver");
       $(this).addClass("hideWord2");
-    } 
+    }
     else if($(this).hasClass("hideWord2")) {
       $(this).removeClass("hideWord2");
     }
     else {
       $(this).addClass("hideWord");
-      
+
     }
-    //make background white 
+    //make background white
   });
 
   // This allows the user to see a word if it's hidden by hovering the
@@ -46,7 +48,7 @@ $(document).ready(function() {
     }
 
   );
-  
+
   $("img").on("click", function(){
     if($(this).hasClass("imageHide")){
       $(this).removeClass("imageHide");
@@ -56,7 +58,7 @@ $(document).ready(function() {
       $(this).addClass("imageHide");
     }
   });
-  
+
   $("img").hover(
     function() {
       if ($(this).hasClass("imageHide")) {
